c9c006b7fa6c41105337451602ec8259ced7e2ab
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-01 22:31:14 -0400

    Writing test for render svg as circle

---
+++ tests/circle.test.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES================
+const Circle = require("../lib/circle");
+
+// Test
+describe("Circle Class", () => {
+  it("Should render a valid SVG with provided arguments", () => {
+    // Examples input user
+    const logoText = "ABC";
+    const logoTextColor = "blue";
+    const shapeColor = "red";
+
+    // New circle object to acces render function
+    const circle = new Circle();
+    const render = circle.render(logoText, logoTextColor, shapeColor);
+
+    expect(render).toEqual(
+      `<svg version="1.1" width="300" height="200"xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${shapeColor}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${logoTextColor}">${logoText}</text></svg>`
+    );
+  });
+});
