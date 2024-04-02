dde91bfb46cd2b0f2e20b421a8333613f1b91c04
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-01 23:25:27 -0400

    Creating readme and license

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -14,7 +14,7 @@ describe("Circle Class", () => {
     const render = circle.render(logoText, logoTextColor, shapeColor);
 
     expect(render).toEqual(
-      `<svg version="1.1" width="300" height="200"xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${shapeColor}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${logoTextColor}">${logoText}</text></svg>`
+      `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${shapeColor}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${logoTextColor}">${logoText}</text></svg>`
     );
   });
 });
